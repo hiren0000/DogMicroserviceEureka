657c0def7e30cb22d3f4844ba4912021382fd54c
Hiren <rebeldevmurari@gmail.com>
2022-12-05 18:27:59 +0000

    Second commit with implemented Microservice module Dog

---
+++ DogMicroserviceClient/src/main/java/com/rebel/DogMicroserviceClient/DogMicroserviceClientApplication.java
@@ -0,0 +1,13 @@
+package com.rebel.DogMicroserviceClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogMicroserviceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogMicroserviceClientApplication.class, args);
+	}
+
+}
